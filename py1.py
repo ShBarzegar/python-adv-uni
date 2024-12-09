866dd5c5b3ee2b6491f2a38d95b4f77164ffcd67
ShBarzegar <shayanbarzegar406@gmail.com>
2024-12-09 14:03:32 +0330

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,38 +1,37 @@
 import requests as r
 class Fetcher:
     def __init__(self):
-        self.students =[]
-        self.l = r.get("https://cdn.ituring.ir/ex/users.json").json()
-        for student in self.l:
-             moshakhasat = {"esm":student["name"] , "famili":student["last_name"] , "ghad":student["height"] , "wazn":student["weight"] , "nomreh":student["score"] , "pasandaz":student["savings"] , "dast mozd":student["salary"] ,"shahr":student["city"] , "ostan":student["province"] , "arze joghrafiaie":student["latitude"] , "tolejoghrafiaei":student["longitude"]}     
-             self.students.append(moshakhasat)
+        
+        self.students = r.get("https://cdn.ituring.ir/ex/users.json").json()
+        
+        
     def nerds(self):
         nerdsnames = set()
         for student in self.students:
-            if student["nomreh"] > 18.5:
-                nerdsnames.add(student["esm"] + " " + student["famili"])
+            if student["score"] > 18.5:
+                nerdsnames.add(student["name"] + " " + student["last_name"])
         print("bache zerangha:", nerdsnames)
     def sultans(self):
         bishtarinnomreh = 0
         sultans = () 
         for student in self.students:
-            if student["nomreh"] > bishtarinnomreh:
-                bishtarinnomreh = student["nomreh"]
-                sultans = (student["esm"] + " " + student["famili"],)  
-            elif student["nomreh"] == bishtarinnomreh:
-                sultans += (student["esm"] + " " + student["famili"],)  
+            if student["score"] > bishtarinnomreh:
+                bishtarinnomreh = student["score"]
+                sultans = (student["name"] + " " + student["last_name"],)  
+            elif student["score"] == bishtarinnomreh:
+                sultans += (student["name"] + " " + student["last_name"],)  
         print("salatin:", sultans)
     def mean(self):
         tedadenafarat = len(self.students) 
         jamenomreha = 0  
         for student in self.students:
-            jamenomreha += float(student["nomreh"])  
+            jamenomreha += float(student["score"])  
         miangin = jamenomreha / tedadenafarat
         print("miangin nomreha:", miangin)
     def get_students(self):
         listedaneshjouha = []
         for student in self.students:
-            dictionarydaneshjou = {"esm":student["esm"] , "famili":student["famili"] , "ghad":student["ghad"] , "wazn":student["wazn"] , "nomreh":student["nomreh"] , "pasandaz":student["pasandaz"] , "dast mozd":student["dast mozd"]}    
+            dictionarydaneshjou = {"name":student["name"] , "last_name":student["last_name"] , "height":student["height"] , "weight":student["weight"] , "score":student["score"] , "savings":student["savings"] , "salary":student["salary"]}    
             listedaneshjouha.append(dictionarydaneshjou)
         print("liste daneshjouha:", listedaneshjouha)
         return listedaneshjouha
